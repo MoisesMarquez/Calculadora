5f971f8fbc1a2d4f84ea8eeda54544c03a1f0e9a
MoyMarquez <martin_mmc@outlook.com>
2019-02-06 09:12:41 -0600

    agregando una prueba mas

--- TestCalculadora.py
+++ TestCalculadora.py
@@ -8,5 +8,9 @@ class TestCalculadora(unittest.TestCase):
         calc = Calculadora()
         self.assertEquals(4,calc.suma(2,2))
 
+    def test_suma_5_mas_10(self):
+        calc = Calculadora()
+        self.assertEquals(15,calc.suma(5,10))
+
 if __name__ == '__main__':
     unittest.main()
